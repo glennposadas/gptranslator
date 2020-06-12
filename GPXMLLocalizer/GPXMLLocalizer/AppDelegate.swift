eace65c357600323dc8a9d4b166788ac1974d69a
Glenn Posadas <hello@glennvon.com>
2020-06-13 00:24:14 +0800

    add base project.

---
+++ GPXMLLocalizer/GPXMLLocalizer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GPXMLLocalizer
+//
+//  Created by Glenn Von Posadas on 6/13/20.
+//  Copyright © 2020 GAM Dynamics. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GPXMLLocalizer/GPXMLLocalizer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GPXMLLocalizer
+//
+//  Created by Glenn Von Posadas on 6/13/20.
+//  Copyright © 2020 GAM Dynamics. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
