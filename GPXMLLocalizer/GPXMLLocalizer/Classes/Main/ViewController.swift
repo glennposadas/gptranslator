1a460028ff7200997237ce4814c5ffc84b26894e
Glenn Posadas <hello@glennvon.com>
2020-06-13 22:07:44 +0800

    add animations and alerts.

--- GPXMLLocalizer/GPXMLLocalizer/Classes/Main/ViewController.swift
+++ GPXMLLocalizer/GPXMLLocalizer/Classes/Main/ViewController.swift
@@ -20,12 +20,24 @@ class ViewController: NSViewController {
     private var englishPaths = [URL]()
     private var italianPaths = [URL]()
     
+    private var englishDictionary = [String : Any]()
+    private var englishKeys = [String]()
+    private var englishValues = [String]()
+    
+    // MARK: Views
+    
+    @IBOutlet weak var selectEnglishFilesBtn: NSButton!
+    @IBOutlet weak var selectItalianFilesBtn: NSButton!
+    @IBOutlet weak var generateBtn: NSButton!
+    @IBOutlet weak var loader: NSProgressIndicator!
+    
     // MARK: - Functions
     // MARK: Overrides
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.setupUI()
     }
     
     private func handleSelectionOfFiles(withBlock completion: SelectedFilesBlock) {
@@ -40,8 +52,35 @@ class ViewController: NSViewController {
             completion?(result)
             return
         }
-
+        
         completion?([])
+    }
+    
+    private func setupUI() {
+        self.loader.alphaValue = 0
+    }
+    
+    private func showCompletionAlert() {
+        let alert = NSAlert()
+        alert.messageText = "Success parsing!"
+        alert.informativeText = "You may see the output in your desktop folder."
+        alert.addButton(withTitle: "OK")
+        alert.alertStyle = .informational
+        
+        var w: NSWindow?
+        if let window = view.window {
+            w = window
+        }
+        else if let window = NSApplication.shared.windows.first {
+            w = window
+        }
+        if let window = w {
+            alert.beginSheetModal(for: window){ (modalResponse) in
+                if modalResponse == .alertFirstButtonReturn {
+                    print("Document deleted")
+                }
+            }
+        }
     }
     
     // MARK: IBActions
@@ -63,17 +102,49 @@ class ViewController: NSViewController {
         self.handleSelectionOfFiles { urls in
             self.italianPaths = urls
         }
+        
     }
 }
 
 extension ViewController: XMLParserDelegate {
+    func parserDidStartDocument(_ parser: XMLParser) {
+        self.generateBtn.wantsLayer = true
+        self.loader.wantsLayer = true
+        self.loader.startAnimation(self)
+        
+        NSAnimationContext.runAnimationGroup({ context in
+            context.duration = 0.3
+            self.loader.alphaValue = 1.0
+        }, completionHandler: {
+            self.generateBtn.alphaValue = 0
+        })
+    }
+    
+    func parserDidEndDocument(_ parser: XMLParser) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)) {
+            self.generateBtn.wantsLayer = true
+            self.loader.wantsLayer = true
+            self.loader.stopAnimation(self)
+            
+            NSAnimationContext.runAnimationGroup({ context in
+                context.duration = 0.3
+                self.generateBtn.alphaValue = 1.0
+            }, completionHandler: {
+                self.loader.alphaValue = 0
+                self.showCompletionAlert()
+            })
+        }
+    }
+    
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         print("Found characters!!! \(string)")
+        self.englishValues.append(string)
     }
     
     func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
-        print("didStartElement: \(elementName)")
-        print("qualifiedName: \(qName)")
-        print("attributes: \(attributeDict)")
+        guard let key = attributeDict["name"] else { return }
+        self.englishKeys.append(key)
     }
+    
+    
 }
--- GPXMLLocalizer/GPXMLLocalizer/Classes/Parser/Parser.swift
+++ GPXMLLocalizer/GPXMLLocalizer/Classes/Parser/Parser.swift
@@ -7,35 +7,35 @@
 //
 
 import Foundation
-
-class Parser {
-    
-    // MARK: - Properties
-    
-    private var parserDelegate: ParserDelegate!
-    
-    private var xmlData: Data!
-    var returnValue: String?
-    
-    // MARK: - Functions
-    
-    init(xmlData: Data, element: String) {
-        self.xmlString = xml
-        self.parserDelegate = ParserDelegate(element: element)
-    }
-    
-    
-    func parse() -> Bool{
-        let data = self.xmlString.dataU
-        
-        let parser = XMLParser(data: data)//Parser(data: xmlString.dataUsingEncoding(.utf8, allowLossyConversion: false))
-        parser.delegate = self.parserDelegate
-        
-        if parser.parse() {
-            self.returnValue = self.parserDelegate.text
-            return true
-        }
-        
-        return false
-    }
-}
+//
+//class Parser {
+//    
+//    // MARK: - Properties
+//    
+//    private var parserDelegate: ParserDelegate!
+//    
+//    private var xmlData: Data!
+//    var returnValue: String?
+//    
+//    // MARK: - Functions
+//    
+//    init(xmlData: Data, element: String) {
+//        self.xmlString = xml
+//        self.parserDelegate = ParserDelegate(element: element)
+//    }
+//    
+//    
+//    func parse() -> Bool{
+//        let data = self.xmlString.dataU
+//        
+//        let parser = XMLParser(data: data)//Parser(data: xmlString.dataUsingEncoding(.utf8, allowLossyConversion: false))
+//        parser.delegate = self.parserDelegate
+//        
+//        if parser.parse() {
+//            self.returnValue = self.parserDelegate.text
+//            return true
+//        }
+//        
+//        return false
+//    }
+//}
