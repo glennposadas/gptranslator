ac30a1260fe25a534625c811ef7dd35d5327a57e
Glenn Posadas <hello@glennvon.com>
2020-06-14 01:10:17 +0800

    improve isValidValue.

--- GPXMLLocalizer/GPXMLLocalizer/Classes/Parser/String+Parser.swift
+++ GPXMLLocalizer/GPXMLLocalizer/Classes/Parser/String+Parser.swift
@@ -11,26 +11,31 @@ import Foundation
 extension String {
     /// Validates the value. TODO: Use Regex.
     var isValidValue: Bool {
-        print(self)
-        if self.isEmpty || self == "\n    " || self == "\n\t" {
+        if self.isEmpty {
             return false
         }
         
-        return true
+        var copy = self
+        
+        copy = copy.replacingOccurrences(of: "\n", with: "")
+        copy = copy.replacingOccurrences(of: "\t", with: "")
+        copy = copy.replacingOccurrences(of: " ", with: "")
+        
+        return !copy.isEmpty
     }
     
     /**
-    Replaces the php-like placeholders.
-    - ref: https://stackoverflow.com/questions/62361346/convert-placeholders-such-as-1s-to-x-in-swift
-    */
+     Replaces the php-like placeholders.
+     - ref: https://stackoverflow.com/questions/62361346/convert-placeholders-such-as-1s-to-x-in-swift
+     */
     mutating func replacePlaceholders() {
         let pattern = #"%(\d*)\$s"#
         var range = self.range(of: pattern, options: .regularExpression)
-
+        
         while range != nil {
             let placeholder = self[range!]
             let number = placeholder.trimmingCharacters(in: CharacterSet(charactersIn: "0123456789.").inverted)
-
+            
             if let value = Int(number) {
                 self = self.replacingOccurrences(of: placeholder, with: "{\(value - 1)}")
             }
